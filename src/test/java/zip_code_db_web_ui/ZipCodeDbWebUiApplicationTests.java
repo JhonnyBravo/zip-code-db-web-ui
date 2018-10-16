f6b603be771063b73ba7c6b28500cd0d4de31ca4
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-10-17 02:43:37 +0900

    new file: src/test/java/zip_code_db_web_ui/ZipCodeDbWebUiApplicationTests.java

---
+++ src/test/java/zip_code_db_web_ui/ZipCodeDbWebUiApplicationTests.java
@@ -0,0 +1,16 @@
+package zip_code_db_web_ui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZipCodeDbWebUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
