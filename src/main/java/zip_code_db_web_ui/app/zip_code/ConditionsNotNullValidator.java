e86a5dab47d43444a81c9610658b14167d66420f
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-11-26 21:16:17 +0900

    new file: src/main/java/zip_code_db_web_ui/app/zip_code/ConditionsNotNullValidator.java

---
+++ src/main/java/zip_code_db_web_ui/app/zip_code/ConditionsNotNullValidator.java
@@ -0,0 +1,30 @@
+package zip_code_db_web_ui.app.zip_code;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ConditionsNotNullValidator implements ConstraintValidator<ConditionsNotNull, Object> {
+    private String field;
+    private String message;
+
+    @Override
+    public void initialize(ConditionsNotNull constraintAnnotation) {
+        field = constraintAnnotation.field();
+        message = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        boolean result = true;
+        final ZipCodeForm form = (ZipCodeForm) value;
+
+        if (form.getZipCode().isEmpty() && form.getPrefecture().isEmpty() && form.getCity().isEmpty()
+                && form.getArea().isEmpty()) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(message).addPropertyNode(field).addConstraintViolation();
+            result = false;
+        }
+
+        return result;
+    }
+}
