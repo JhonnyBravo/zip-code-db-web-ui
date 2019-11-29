05f99f13b24ba096d837e9f06ba7b59e8a25d315
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-11-30 02:10:34 +0900

    modified: src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceImpl.java
    mybatis の導入に伴う修正。
    PrefectureService の機能を統合。

--- src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceImpl.java
+++ src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceImpl.java
@@ -1,35 +1,46 @@
 package zip_code_db_web_ui.domain.service.zip_code;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
-import java_itamae_connection.domain.model.ConnectionInfo;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import zip_code_db_cli.domain.model.ZipCode;
 import zip_code_db_web_ui.domain.repository.zip_code.ZipCodeRepository;
-import zip_code_db_web_ui.domain.repository.zip_code.ZipCodeRepositoryImpl;
 
-public class ZipCodeServiceImpl extends ZipCodeService {
-    private final ConnectionInfo cnInfo;
-    private final ZipCodeRepository zcr;
+public class ZipCodeServiceImpl implements ZipCodeService {
+    private ZipCodeRepository repository;
+    private final SqlSessionFactory factory;
 
-    /**
-     * @param cnInfo DB の接続情報を納めた ConnectionInfo を指定する。
-     */
-    public ZipCodeServiceImpl(ConnectionInfo cnInfo) {
-        this.cnInfo = cnInfo;
-        zcr = new ZipCodeRepositoryImpl();
+    public ZipCodeServiceImpl() throws Exception {
+        factory = new SqlSessionFactoryBuilder()
+                .build(Resources.getResourceAsStream("mybatis-config.xml"));
     }
 
     @Override
-    public List<ZipCode> find(ZipCode zipcode) throws Exception {
+    public List<ZipCode> find(ZipCode zipcode) {
         List<ZipCode> recordset = new ArrayList<>();
 
-        try (Connection connection = getConnection(cnInfo)) {
-            recordset = zcr.find(connection, zipcode);
+        try (SqlSession session = factory.openSession()) {
+            repository = session.getMapper(ZipCodeRepository.class);
+            recordset = repository.find(zipcode);
         }
 
         return recordset;
     }
 
+    @Override
+    public List<String> findPrefectureAll() {
+        List<String> recordset = new ArrayList<>();
+
+        try (SqlSession session = factory.openSession()) {
+            repository = session.getMapper(ZipCodeRepository.class);
+            recordset = repository.findPrefectureAll();
+        }
+
+        return recordset;
+    }
 }
