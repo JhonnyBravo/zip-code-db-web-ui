b5aa134ae22c0050e764cf70d7d543c88617f962
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-11-30 02:17:21 +0900

    modified: src/test/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepositoryTest.java
    mybatis の導入により SQL の生成処理が不要となった為、テストの内容を変更。

--- src/test/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepositoryTest.java
+++ src/test/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepositoryTest.java
@@ -3,7 +3,14 @@ package zip_code_db_web_ui.domain.repository.zip_code;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Before;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
@@ -12,181 +19,303 @@ import zip_code_db_cli.domain.model.ZipCode;
 
 @RunWith(Enclosed.class)
 public class ZipCodeRepositoryTest {
-    public static class パラメータを一つ指定する場合 {
+    public static class パラメータを指定しない場合 {
+        private static SqlSessionFactory factory;
         private ZipCodeRepository repository;
 
-        @Before
-        public void setUp() throws Exception {
-            repository = new ZipCodeRepositoryImpl();
+        @BeforeClass
+        public static void setUp() throws Exception {
+            factory = new SqlSessionFactoryBuilder()
+                    .build(Resources.getResourceAsStream("mybatis-config.xml"));
         }
 
         @Test
-        public void パラメータを指定しない場合() {
+        public void testFind() {
             final ZipCode zipcode = new ZipCode();
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code LIMIT 1000;"));
+            List<ZipCode> recordset = new ArrayList<>();
+
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(124329));
+        }
+
+        @Test
+        public void testFindPrefectureAll() {
+            List<String> recordset = new ArrayList<>();
+
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.findPrefectureAll();
+            }
+
+            assertThat(recordset.size(), is(47));
+        }
+    }
+
+    public static class パラメータを一つ指定する場合 {
+        private static SqlSessionFactory factory;
+        private ZipCodeRepository repository;
+
+        @BeforeClass
+        public static void setUp() throws Exception {
+            factory = new SqlSessionFactoryBuilder()
+                    .build(Resources.getResourceAsStream("mybatis-config.xml"));
         }
 
         @Test
         public void 郵便番号を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setZipCode("0640941");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE zip_code=? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
 
         @Test
         public void 都道府県名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setPrefecture("北海道");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE prefecture=? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(8248));
         }
 
         @Test
         public void 市区郡名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setCity("札幌市中央区");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE city LIKE ? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(89));
         }
 
         @Test
         public void 町域名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setArea("旭ケ丘");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE area LIKE ? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(33));
         }
     }
 
     public static class パラメータを複数指定する場合 {
+        private static SqlSessionFactory factory;
         private ZipCodeRepository repository;
 
-        @Before
-        public void setUp() throws Exception {
-            repository = new ZipCodeRepositoryImpl();
+        @BeforeClass
+        public static void setUp() throws Exception {
+            factory = new SqlSessionFactoryBuilder()
+                    .build(Resources.getResourceAsStream("mybatis-config.xml"));
         }
 
         @Test
         public void 市区郡名と町域名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setCity("札幌市中央区");
             zipcode.setArea("旭ケ丘");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE city LIKE ? AND area LIKE ? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
 
         @Test
         public void 都道府県名と町域名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setPrefecture("北海道");
             zipcode.setArea("旭ケ丘");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE prefecture=? AND area LIKE ? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(4));
         }
 
         @Test
         public void 都道府県名と市区郡名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setPrefecture("北海道");
             zipcode.setCity("札幌市中央区");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE prefecture=? AND city LIKE ? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(89));
         }
 
         @Test
         public void 都道府県名_市区郡名_町域名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setPrefecture("北海道");
             zipcode.setCity("札幌市中央区");
             zipcode.setArea("旭ケ丘");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql,
-                    is("SELECT * FROM t_zip_code WHERE prefecture=? AND city LIKE ? AND area LIKE ? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
 
         @Test
         public void 郵便番号と町域名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setZipCode("0640941");
             zipcode.setArea("旭ケ丘");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE zip_code=? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
 
         @Test
         public void 郵便番号と市区郡名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setZipCode("0640941");
             zipcode.setCity("札幌市中央区");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE zip_code=? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
 
         @Test
         public void 郵便番号_市区郡名_町域名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setZipCode("0640941");
             zipcode.setCity("札幌市中央区");
             zipcode.setArea("旭ケ丘");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE zip_code=? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
 
         @Test
         public void 郵便番号と都道府県名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setPrefecture("北海道");
             zipcode.setZipCode("0640941");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE zip_code=? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
 
         @Test
         public void 郵便番号_都道府県名_町域名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setZipCode("0640941");
             zipcode.setPrefecture("北海道");
             zipcode.setArea("旭ケ丘");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE zip_code=? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
 
         @Test
         public void 郵便番号_都道府県名_市区郡名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setZipCode("0640941");
             zipcode.setPrefecture("北海道");
             zipcode.setCity("札幌市中央区");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE zip_code=? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
 
         @Test
         public void 郵便番号_都道府県名_市区郡名_町域名を指定した場合() {
+            List<ZipCode> recordset = new ArrayList<>();
+
             final ZipCode zipcode = new ZipCode();
             zipcode.setZipCode("0640941");
             zipcode.setPrefecture("北海道");
             zipcode.setCity("札幌市中央区");
             zipcode.setArea("旭ケ丘");
 
-            final String sql = repository.getSql(zipcode);
-            assertThat(sql, is("SELECT * FROM t_zip_code WHERE zip_code=? LIMIT 1000;"));
+            try (SqlSession session = factory.openSession()) {
+                repository = session.getMapper(ZipCodeRepository.class);
+                recordset = repository.find(zipcode);
+            }
+
+            assertThat(recordset.size(), is(1));
         }
     }
 }
