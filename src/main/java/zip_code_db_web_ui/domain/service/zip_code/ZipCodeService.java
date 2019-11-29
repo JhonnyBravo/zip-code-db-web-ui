8045829c20f09a9dcfb18c4c9d469f587f200414
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-11-30 02:07:08 +0900

    modified: src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeService.java
    mybatis の導入により Connection の管理方式が変わった為、
    ConnectionService からの継承を廃止。
    
    PrefectureService の機能を統合。

--- src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeService.java
+++ src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeService.java
@@ -2,19 +2,23 @@ package zip_code_db_web_ui.domain.service.zip_code;
 
 import java.util.List;
 
-import java_itamae_connection.domain.service.connection.ConnectionService;
 import zip_code_db_cli.domain.model.ZipCode;
 
 /**
  * 住所検索を実行するサービス
  */
-public abstract class ZipCodeService extends ConnectionService {
+public interface ZipCodeService {
     /**
      * 住所検索を実行し、結果を List に変換して返す。
-     * 
-     * @param zipcode パラメータとして渡す ZipCode を指定する。
+     *
+     * @param zipcode
+     *            パラメータとして渡す ZipCode を指定する。
      * @return recordset 検索結果を List に変換して返す。
-     * @throws Exception {@link java.lang.Exception}
      */
-    public abstract List<ZipCode> find(ZipCode zipcode) throws Exception;
+    public List<ZipCode> find(ZipCode zipcode);
+
+    /**
+     * @return recordset 都道府県名の一覧を返す。
+     */
+    public List<String> findPrefectureAll();
 }
