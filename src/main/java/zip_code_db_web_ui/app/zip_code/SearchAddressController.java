63eae980160f025a4ba363695e4dd1c48162466c
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-03-16 22:36:31 +0900

    modified: src/main/java/zip_code_db_web_ui/app/zip_code/SearchAddressController.java
    パッケージのバージョンアップに伴う import の修正。

--- src/main/java/zip_code_db_web_ui/app/zip_code/SearchAddressController.java
+++ src/main/java/zip_code_db_web_ui/app/zip_code/SearchAddressController.java
@@ -12,10 +12,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import zip_code_db_cli.domain.model.ZipCode;
 import zip_code_db_web_ui.domain.service.zip_code.ZipCodeService;
 import zip_code_db_web_ui.domain.service.zip_code.ZipCodeServiceImpl;
@@ -32,8 +32,7 @@ public class SearchAddressController extends HttpServlet {
 
     /**
      * @see HttpServlet#HttpServlet()
-     * @throws Exception
-     *             {@link java.lang.Exception}
+     * @throws Exception {@link java.lang.Exception}
      */
     public SearchAddressController() throws Exception {
         super();
@@ -44,20 +43,16 @@ public class SearchAddressController extends HttpServlet {
     /**
      * 住所検索を実行する
      *
-     * @param request
-     *            下記のリクエストパラメータを受け取る。
-     *            <ul>
-     *            <li>zip_code: 検索対象とする郵便番号を指定する。</li>
-     *            <li>prefecture: 検索対象とする都道府県名を指定する。</li>
-     *            <li>city: 検索対象とする市区郡名を指定する。</li>
-     *            <li>area: 検索対象とする町域名を指定する。</li>
-     *            </ul>
-     * @param response
-     *            検索結果を表示ページへ転送する。
-     * @throws ServletException
-     *             {@link javax.servlet.ServletException}
-     * @throws IOException
-     *             {@link java.io.IOException}
+     * @param request  下記のリクエストパラメータを受け取る。
+     *                 <ul>
+     *                 <li>zip_code: 検索対象とする郵便番号を指定する。</li>
+     *                 <li>prefecture: 検索対象とする都道府県名を指定する。</li>
+     *                 <li>city: 検索対象とする市区郡名を指定する。</li>
+     *                 <li>area: 検索対象とする町域名を指定する。</li>
+     *                 </ul>
+     * @param response 検索結果を表示ページへ転送する。
+     * @throws ServletException {@link javax.servlet.ServletException}
+     * @throws IOException      {@link java.io.IOException}
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
      *      response)
      */
