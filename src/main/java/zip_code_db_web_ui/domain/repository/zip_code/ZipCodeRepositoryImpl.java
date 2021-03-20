6800a513a9aca8353d4d47d78aa8b4713996c317
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-03-21 02:17:20 +0900

    modified: src/main/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepositoryImpl.java
    条件分岐の記述を修正。
    値が null である場合でもエラーが発生しないように修正。

--- src/main/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepositoryImpl.java
+++ src/main/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepositoryImpl.java
@@ -2,6 +2,7 @@ package zip_code_db_web_ui.domain.repository.zip_code;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -19,26 +20,36 @@ import zip_code_db_web_ui.domain.model.ZipCode;
 public class ZipCodeRepositoryImpl implements ZipCodeRepository {
     @Autowired
     private NamedParameterJdbcTemplate template;
+    private final Predicate<String> isNotEmpty = value -> {
+        boolean result = false;
+
+        if (value != null && !value.isEmpty()) {
+            result = true;
+        }
+
+        return result;
+    };
 
     @Override
     public String getSql(ZipCode model) {
-        final List<String> conditions = new ArrayList<String>();
+        final List<String> conditions = new ArrayList<>();
         final StringBuilder buffer = new StringBuilder();
 
         buffer.append("SELECT * FROM t_zip_code");
 
-        if (!model.getZipCode().isEmpty()) {
+        if (isNotEmpty.test(model.getZipCode())) {
             buffer.append(" WHERE zip_code = :zip_code");
-        } else if (!model.getPrefecture().isEmpty() || !model.getCity().isEmpty() || !model.getArea().isEmpty()) {
-            if (!model.getPrefecture().isEmpty()) {
+        } else if (isNotEmpty.test(model.getPrefecture()) || isNotEmpty.test(model.getCity())
+                || isNotEmpty.test(model.getArea())) {
+            if (isNotEmpty.test(model.getPrefecture())) {
                 conditions.add("prefecture = :prefecture");
             }
 
-            if (!model.getCity().isEmpty()) {
+            if (isNotEmpty.test(model.getCity())) {
                 conditions.add("city LIKE :city");
             }
 
-            if (!model.getArea().isEmpty()) {
+            if (isNotEmpty.test(model.getArea())) {
                 conditions.add("area LIKE :area");
             }
 
@@ -56,23 +67,24 @@ public class ZipCodeRepositoryImpl implements ZipCodeRepository {
         final String sql = getSql(model);
         final MapSqlParameterSource params = new MapSqlParameterSource();
 
-        if (!model.getZipCode().isEmpty()) {
+        if (isNotEmpty.test(model.getZipCode())) {
             params.addValue("zip_code", model.getZipCode());
-        } else if (!model.getPrefecture().isEmpty() || !model.getCity().isEmpty() || !model.getArea().isEmpty()) {
-            if (!model.getPrefecture().isEmpty()) {
+        } else if (isNotEmpty.test(model.getPrefecture()) || isNotEmpty.test(model.getCity())
+                || isNotEmpty.test(model.getArea())) {
+            if (isNotEmpty.test(model.getPrefecture())) {
                 params.addValue("prefecture", model.getPrefecture());
             }
 
-            if (!model.getCity().isEmpty()) {
+            if (isNotEmpty.test(model.getCity())) {
                 params.addValue("city", model.getCity());
             }
 
-            if (!model.getArea().isEmpty()) {
+            if (isNotEmpty.test(model.getArea())) {
                 params.addValue("area", model.getArea());
             }
         }
 
-        final RowMapper<ZipCode> mapper = new BeanPropertyRowMapper<ZipCode>(ZipCode.class);
+        final RowMapper<ZipCode> mapper = new BeanPropertyRowMapper<>(ZipCode.class);
         final List<ZipCode> recordset = template.query(sql, params, mapper);
         return recordset;
     }
