ba8ed34d7fc930dd7aecec0a1c5cb5028a1c7691
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-03-22 01:37:05 +0900

    modified: src/main/java/zip_code_db_web_ui/ZipCodeDbWebUiApplication.java
    scanBasePackages の設定を追記。

--- src/main/java/zip_code_db_web_ui/ZipCodeDbWebUiApplication.java
+++ src/main/java/zip_code_db_web_ui/ZipCodeDbWebUiApplication.java
@@ -3,10 +3,10 @@ package zip_code_db_web_ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "zip_code_db_web_ui")
 public class ZipCodeDbWebUiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipCodeDbWebUiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipCodeDbWebUiApplication.class, args);
+    }
 }
