a32ed645a45d447cae63d58d04565a0e7b2857f4
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-03-22 01:03:37 +0900

    new file: src/test/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceTest.java

---
+++ src/test/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceTest.java
@@ -0,0 +1,177 @@
+package zip_code_db_web_ui.domain.service.zip_code;
+
+import static org.junit.Assert.assertThat;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import zip_code_db_web_ui.domain.model.ZipCode;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZipCodeServiceTest {
+    @Autowired
+    private ZipCodeService service;
+
+    @Test
+    public void testFind郵便番号のみを指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setZipCode("0800030");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(1));
+
+        final ZipCode record = recordset.get(0);
+        assertThat(record.getZipCode(), is("0800030"));
+        assertThat(record.getPrefecture(), is("北海道"));
+        assertThat(record.getCity(), is("帯広市"));
+        assertThat(record.getArea(), is("大通北"));
+    }
+
+    @Test
+    public void testFind都道府県名のみを指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setPrefecture("北海道");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(8252));
+    }
+
+    @Test
+    public void testFind市区郡名のみを指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setCity("札幌市%");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(823));
+    }
+
+    @Test
+    public void testFind町域名のみを指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setArea("大通%");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(50));
+    }
+
+    @Test
+    public void testFind都道府県名と市区郡名と町域名を指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setPrefecture("北海道");
+        zipcode.setCity("札幌市%");
+        zipcode.setArea("大通%");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(3));
+    }
+
+    @Test
+    public void testFind都道府県名と市区郡名を指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setPrefecture("北海道");
+        zipcode.setCity("札幌市中央区");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(89));
+    }
+
+    @Test
+    public void testFind都道府県名と町域名を指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setPrefecture("北海道");
+        zipcode.setArea("大通%");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(38));
+    }
+
+    @Test
+    public void testFind市区郡名と町域名を指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setCity("札幌市%");
+        zipcode.setArea("大通東");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(1));
+
+        final ZipCode record = recordset.get(0);
+        assertThat(record.getZipCode(), is("0600041"));
+        assertThat(record.getPrefecture(), is("北海道"));
+        assertThat(record.getCity(), is("札幌市中央区"));
+        assertThat(record.getArea(), is("大通東"));
+    }
+
+    @Test
+    public void testFind郵便番号と都道府県名を指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setZipCode("0600041");
+        zipcode.setPrefecture("北海道");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(1));
+
+        final ZipCode record = recordset.get(0);
+        assertThat(record.getZipCode(), is("0600041"));
+        assertThat(record.getPrefecture(), is("北海道"));
+        assertThat(record.getCity(), is("札幌市中央区"));
+        assertThat(record.getArea(), is("大通東"));
+    }
+
+    @Test
+    public void testFind郵便番号と市区郡名を指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setZipCode("0600041");
+        zipcode.setCity("札幌市%");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(1));
+
+        final ZipCode record = recordset.get(0);
+        assertThat(record.getZipCode(), is("0600041"));
+        assertThat(record.getPrefecture(), is("北海道"));
+        assertThat(record.getCity(), is("札幌市中央区"));
+        assertThat(record.getArea(), is("大通東"));
+    }
+
+    @Test
+    public void testFind郵便番号と町域名を指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setZipCode("0600041");
+        zipcode.setArea("大通%");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(1));
+
+        final ZipCode record = recordset.get(0);
+        assertThat(record.getZipCode(), is("0600041"));
+        assertThat(record.getPrefecture(), is("北海道"));
+        assertThat(record.getCity(), is("札幌市中央区"));
+        assertThat(record.getArea(), is("大通東"));
+    }
+
+    @Test
+    public void testFind郵便番号と都道府県名と市区郡名と町域名を指定した場合() {
+        final ZipCode zipcode = new ZipCode();
+        zipcode.setZipCode("0600041");
+        zipcode.setPrefecture("北海道");
+        zipcode.setCity("札幌市%");
+        zipcode.setArea("大通%");
+
+        final List<ZipCode> recordset = service.find(zipcode);
+        assertThat(recordset.size(), is(1));
+
+        final ZipCode record = recordset.get(0);
+        assertThat(record.getZipCode(), is("0600041"));
+        assertThat(record.getPrefecture(), is("北海道"));
+        assertThat(record.getCity(), is("札幌市中央区"));
+        assertThat(record.getArea(), is("大通東"));
+    }
+}
