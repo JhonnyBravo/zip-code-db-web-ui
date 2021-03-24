c40d0f44ebf310b51aa03a7d32bed3f06bb964cf
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-03-25 01:23:29 +0900

    modified: src/main/java/zip_code_db_web_ui/Main.java
    Tomcat への war ファイルデプロイ時に呼び出される初期化処理を追加。

--- src/main/java/zip_code_db_web_ui/Main.java
+++ src/main/java/zip_code_db_web_ui/Main.java
@@ -2,17 +2,24 @@ package zip_code_db_web_ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * アプリケーションを起動する。
  */
 @SpringBootApplication(scanBasePackages = "zip_code_db_web_ui")
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
     /**
      * @param args 起動パラメータとして使用する引数を受け取る。
      */
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
     }
 }
