96d707ee3fdaf8f6f1252ed2a19a03fd5b054e6b
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-03-25 02:21:34 +0900

    modified: src/main/java/zip_code_db_web_ui/ZipCodeDbWebUiApplication.java
    Tomcat への war ファイルデプロイ時に呼び出される初期化処理を追加。

--- src/main/java/zip_code_db_web_ui/ZipCodeDbWebUiApplication.java
+++ src/main/java/zip_code_db_web_ui/ZipCodeDbWebUiApplication.java
@@ -2,11 +2,18 @@ package zip_code_db_web_ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "zip_code_db_web_ui")
-public class ZipCodeDbWebUiApplication {
+public class ZipCodeDbWebUiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ZipCodeDbWebUiApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ZipCodeDbWebUiApplication.class);
     }
 }
