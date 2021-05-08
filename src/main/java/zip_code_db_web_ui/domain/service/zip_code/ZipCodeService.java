725a2f10301819a927704a70d30fc8f67e2f5c00
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-09 01:31:17 +0900

    modified: src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeService.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeService.java
+++ src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeService.java
@@ -1,24 +1,24 @@
 package zip_code_db_web_ui.domain.service.zip_code;
 
 import java.util.List;
-
 import zip_code_db_cli.domain.model.ZipCode;
 
 /**
  * 住所検索を実行するサービス
  */
 public interface ZipCodeService {
-    /**
-     * 住所検索を実行し、結果を List に変換して返す。
-     *
-     * @param zipcode
-     *            パラメータとして渡す ZipCode を指定する。
-     * @return recordset 検索結果を List に変換して返す。
-     */
-    public List<ZipCode> find(ZipCode zipcode);
+  /**
+   * 住所検索を実行し、結果を {@link ZipCode} の {@link List} に変換して返す。
+   *
+   * @param zipcode 検索条件として渡す {@link ZipCode} を指定する。
+   * @return recordset 検索結果を収めた {@link ZipCode} の {@link List} に変換して返す。
+   */
+  List<ZipCode> find(ZipCode zipcode);
 
-    /**
-     * @return recordset 都道府県名の一覧を返す。
-     */
-    public List<String> findPrefectureAll();
+  /**
+   * 都道府県名の一覧を返す。
+   *
+   * @return recordset 都道府県名の一覧
+   */
+  List<String> findPrefectureAll();
 }
