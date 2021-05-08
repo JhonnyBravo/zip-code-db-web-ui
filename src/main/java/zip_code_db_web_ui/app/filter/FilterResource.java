f2299cc3866af085dc26ef19ba05c06159d9d350
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-09 01:33:11 +0900

    modified: src/main/java/zip_code_db_web_ui/app/filter/FilterResource.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/app/filter/FilterResource.java
+++ src/main/java/zip_code_db_web_ui/app/filter/FilterResource.java
@@ -1,7 +1,6 @@
 package zip_code_db_web_ui.app.filter;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -16,29 +15,27 @@ import javax.servlet.annotation.WebFilter;
 @WebFilter("/*")
 public class FilterResource implements Filter {
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-    }
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {}
 
-    /**
-     * 文字エンコーディングとコンテンツタイプを自動設定する。
-     *
-     * @param request  {@link javax.servlet.ServletRequest}
-     * @param response {@link javax.servlet.ServletResponse}
-     * @param chain    {@link javax.servlet.FilterChain}
-     * @throws IOException      {@link java.io.IOException}
-     * @throws ServletException {@link javax.servlet.ServletException}
-     */
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        final String encoding = "UTF-8";
-        request.setCharacterEncoding(encoding);
-        response.setContentType("text/html; charset=" + encoding);
-        chain.doFilter(request, response);
-    }
+  /**
+   * 文字エンコーディングとコンテンツタイプを自動設定する。
+   *
+   * @param request {@link javax.servlet.ServletRequest}
+   * @param response {@link javax.servlet.ServletResponse}
+   * @param chain {@link javax.servlet.FilterChain}
+   * @throws IOException {@link java.io.IOException}
+   * @throws ServletException {@link javax.servlet.ServletException}
+   */
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    final String encoding = "UTF-8";
+    request.setCharacterEncoding(encoding);
+    response.setContentType("text/html; charset=" + encoding);
+    chain.doFilter(request, response);
+  }
 
-    @Override
-    public void destroy() {
-    }
+  @Override
+  public void destroy() {}
 }
