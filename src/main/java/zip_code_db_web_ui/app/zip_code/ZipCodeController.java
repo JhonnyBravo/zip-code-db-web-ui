48c70fc052b8aaf859efb22596f405b309e6f09e
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-09 01:37:21 +0900

    modified: src/main/java/zip_code_db_web_ui/app/zip_code/ZipCodeController.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/app/zip_code/ZipCodeController.java
+++ src/main/java/zip_code_db_web_ui/app/zip_code/ZipCodeController.java
@@ -2,7 +2,6 @@ package zip_code_db_web_ui.app.zip_code;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,7 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import zip_code_db_web_ui.domain.model.Prefecture;
 import zip_code_db_web_ui.domain.service.zip_code.ZipCodeService;
 import zip_code_db_web_ui.domain.service.zip_code.ZipCodeServiceImpl;
@@ -20,53 +18,49 @@ import zip_code_db_web_ui.domain.service.zip_code.ZipCodeServiceImpl;
  */
 @WebServlet("/home")
 public class ZipCodeController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private final ZipCodeHelper helper;
-    private final ZipCodeService service;
+  private final ZipCodeHelper helper;
+  private final ZipCodeService service;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     * @throws Exception
-     *             {@link java.lang.Exception}
-     */
-    public ZipCodeController() throws Exception {
-        super();
-        helper = new ZipCodeHelper();
-        service = new ZipCodeServiceImpl();
+  /**
+   * 初期化処理を実行する。
+   *
+   * @see HttpServlet#HttpServlet()
+   * @throws Exception {@link java.lang.Exception}
+   */
+  public ZipCodeController() throws Exception {
+    super();
+    helper = new ZipCodeHelper();
+    service = new ZipCodeServiceImpl();
+  }
+
+  /**
+   * 住所検索フォームを表示する。
+   *
+   * @param request {@link javax.servlet.http.HttpServletRequest}
+   * @param response 住所検索フォームを表示する。
+   * @throws ServletException {@link javax.servlet.ServletException}
+   * @throws IOException {@link java.io.IOException}
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    final List<String> recordset = service.findPrefectureAll();
+    final ZipCodeForm form = (ZipCodeForm) request.getAttribute("form");
+
+    String prefecture = null;
+
+    if (form != null) {
+      prefecture = form.getPrefecture();
     }
 
-    /**
-     * 住所検索フォームを表示する。
-     *
-     * @param request
-     *            {@link javax.servlet.http.HttpServletRequest}
-     * @param response
-     *            住所検索フォームを表示する。
-     * @throws ServletException
-     *             {@link javax.servlet.ServletException}
-     * @throws IOException
-     *             {@link java.io.IOException}
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        final List<String> recordset = service.findPrefectureAll();
-        final ZipCodeForm form = (ZipCodeForm) request.getAttribute("form");
+    final List<Prefecture> prefectures = helper.convertToPrefectures(recordset, prefecture);
+    request.setAttribute("prefectures", prefectures);
 
-        String prefecture = null;
-
-        if (form != null) {
-            prefecture = form.getPrefecture();
-        }
-
-        final List<Prefecture> prefectures = helper.convertToPrefectures(recordset, prefecture);
-        request.setAttribute("prefectures", prefectures);
-
-        final ServletContext context = getServletContext();
-        final RequestDispatcher dispatcher = context.getRequestDispatcher("/index.jsp");
-        dispatcher.include(request, response);
-    }
+    final ServletContext context = getServletContext();
+    final RequestDispatcher dispatcher = context.getRequestDispatcher("/index.jsp");
+    dispatcher.include(request, response);
+  }
 }
