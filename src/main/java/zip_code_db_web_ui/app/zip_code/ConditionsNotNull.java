8a90032af9a861a70e3b5f0fe4a3a2e701248a55
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-09 01:35:02 +0900

    modified: src/main/java/zip_code_db_web_ui/app/zip_code/ConditionsNotNull.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/app/zip_code/ConditionsNotNull.java
+++ src/main/java/zip_code_db_web_ui/app/zip_code/ConditionsNotNull.java
@@ -3,31 +3,33 @@ package zip_code_db_web_ui.app.zip_code;
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
 
+/**
+ * {@link ZipCodeForm} を対象に相関チェックを実行する。
+ */
 @Documented
 @Retention(RUNTIME)
-@Target({ TYPE, ANNOTATION_TYPE })
-@Constraint(validatedBy = { ConditionsNotNullValidator.class })
+@Target({TYPE, ANNOTATION_TYPE})
+@Constraint(validatedBy = {ConditionsNotNullValidator.class})
 public @interface ConditionsNotNull {
-    String message() default "{zip_code_db_web_ui.app.zip_code.SearchCondition}";
+  String message() default "{zip_code_db_web_ui.app.zip_code.SearchCondition}";
 
-    Class<?>[] groups() default {};
+  Class<?>[] groups() default {};
 
-    Class<? extends Payload>[] payload() default {};
+  Class<? extends Payload>[] payload() default {};
 
-    String field();
+  String field();
 
-    @Documented
-    @Retention(RUNTIME)
-    @Target({ TYPE, ANNOTATION_TYPE })
-    @interface List {
-        ConditionsNotNull[] value();
-    }
+  @Documented
+  @Retention(RUNTIME)
+  @Target({TYPE, ANNOTATION_TYPE})
+  @interface List {
+    ConditionsNotNull[] value();
+  }
 }
