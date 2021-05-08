48cd0ef56ecf3c8d9599ba7331a18e28416fc9e9
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-09 01:30:17 +0900

    modified: src/main/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepository.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepository.java
+++ src/main/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepository.java
@@ -1,10 +1,8 @@
 package zip_code_db_web_ui.domain.repository.zip_code;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-
 import zip_code_db_cli.domain.model.ZipCode;
 
 /**
@@ -12,17 +10,18 @@ import zip_code_db_cli.domain.model.ZipCode;
  */
 @Mapper
 public interface ZipCodeRepository {
-    /**
-     * 住所検索を実行し、結果を List に変換して返す。
-     *
-     * @param zipcode
-     *            パラメータとして渡す ZipCode を指定する。
-     * @return recordset 検索結果を List に変換して返す。
-     */
-    public List<ZipCode> find(@Param("ZipCode") ZipCode zipcode);
+  /**
+   * 住所検索を実行し、結果を {@link ZipCode} の {@link List} に変換して返す。
+   *
+   * @param zipcode 検索条件として渡す {@link ZipCode} を指定する。
+   * @return recordset 検索結果を収めた {@link ZipCode} の {@link List} を返す。
+   */
+  List<ZipCode> find(@Param("ZipCode") ZipCode zipcode);
 
-    /**
-     * @return recordset 都道府県名の一覧を返す。
-     */
-    public List<String> findPrefectureAll();
+  /**
+   * 都道府県名の一覧を返す。
+   *
+   * @return recordset 都道府県名の一覧
+   */
+  List<String> findPrefectureAll();
 }
