9238882d70c27d65f3c589bbde5bfdee98ad829d
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-11 01:47:39 +0900

    modified: src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceImpl.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceImpl.java
+++ src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceImpl.java
@@ -1,22 +1,20 @@
 package zip_code_db_web_ui.domain.service.zip_code;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import zip_code_db_web_ui.domain.model.ZipCode;
 import zip_code_db_web_ui.domain.repository.zip_code.ZipCodeRepository;
 
 @Service
 public class ZipCodeServiceImpl implements ZipCodeService {
-    @Autowired
-    private ZipCodeRepository repository;
+  @Autowired
+  private ZipCodeRepository repository;
 
-    @Override
-    public List<ZipCode> find(ZipCode zipcode) {
-        final List<ZipCode> recordset = repository.find(zipcode);
-        return recordset;
-    }
+  @Override
+  public List<ZipCode> find(ZipCode zipcode) {
+    final List<ZipCode> recordset = repository.find(zipcode);
+    return recordset;
+  }
 
 }
