152c826ec31955ef75dc3e97925f1dff0c925713
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-11 01:51:37 +0900

    modified: src/main/java/zip_code_db_web_ui/Main.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/Main.java
+++ src/main/java/zip_code_db_web_ui/Main.java
@@ -11,15 +11,17 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication(scanBasePackages = "zip_code_db_web_ui")
 public class Main extends SpringBootServletInitializer {
 
-    /**
-     * @param args 起動パラメータとして使用する引数を受け取る。
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
+  /**
+   * アプリケーションを起動する。
+   *
+   * @param args 起動パラメータとして使用する引数を受け取る。
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
-    }
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Main.class);
+  }
 }
