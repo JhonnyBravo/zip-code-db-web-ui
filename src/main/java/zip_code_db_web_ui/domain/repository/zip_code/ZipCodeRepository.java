a7990c73e225df2a7528a69b051037312d4b86c9
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-11 01:45:05 +0900

    modified: src/main/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepository.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepository.java
+++ src/main/java/zip_code_db_web_ui/domain/repository/zip_code/ZipCodeRepository.java
@@ -1,22 +1,26 @@
 package zip_code_db_web_ui.domain.repository.zip_code;
 
 import java.util.List;
-
 import zip_code_db_web_ui.domain.model.ZipCode;
 
+/**
+ * t_zip_code からのレコード取得を管理する。
+ */
 public interface ZipCodeRepository {
-    /**
-     * @param zipcode パラメータとして渡す ZipCode オブジェクトを指定する。
-     * @return query 動的に生成された SQL 文を返す。
-     */
-    public String getSql(ZipCode zipcode);
+  /**
+   * SQL 文を動的に生成する。
+   *
+   * @param zipcode パラメータとして渡す {@link ZipCode} を指定する。
+   * @return query 動的に生成された SQL 文を返す。
+   */
+  String getSql(ZipCode zipcode);
 
-    /**
-     * レコード検索を実行する。
-     *
-     * @param zipcode パラメータとして渡す ZipCode オブジェクトを指定する。
-     * @return recordset レコードの検索結果をリストに格納して返す。
-     */
-    public List<ZipCode> find(ZipCode zipcode);
+  /**
+   * レコード検索を実行する。
+   *
+   * @param zipcode パラメータとして渡す {@link ZipCode} オブジェクトを指定する。
+   * @return recordset レコードの検索結果を {@link ZipCode} の {@link List} に格納して返す。
+   */
+  List<ZipCode> find(ZipCode zipcode);
 
 }
