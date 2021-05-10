8f293895229180d9c1d821319b70883a593790ad
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-11 01:47:03 +0900

    modified: src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeService.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeService.java
+++ src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeService.java
@@ -1,13 +1,17 @@
 package zip_code_db_web_ui.domain.service.zip_code;
 
 import java.util.List;
-
 import zip_code_db_web_ui.domain.model.ZipCode;
 
+/**
+ * t_zip_code からのレコード取得を管理する。
+ */
 public interface ZipCodeService {
-    /**
-     * @param zipcode 検索パラメータとして受け取る ZipCode を指定する。
-     * @return recordset 住所検索を実行し、結果を List に納めて返す。
-     */
-    public List<ZipCode> find(ZipCode zipcode);
+  /**
+   * 検索条件に合致するレコードを取得する。
+   *
+   * @param zipcode 検索パラメータとして受け取る {@link ZipCode} を指定する。
+   * @return recordset 住所検索を実行し、結果を {@link ZipCode} の {@link List} に納めて返す。
+   */
+  List<ZipCode> find(ZipCode zipcode);
 }
