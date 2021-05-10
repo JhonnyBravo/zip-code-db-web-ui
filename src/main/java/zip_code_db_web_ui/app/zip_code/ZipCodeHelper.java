e0deb4a61066885f08e9ab90279a7fca236d6734
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-05-11 01:49:07 +0900

    modified: src/main/java/zip_code_db_web_ui/app/zip_code/ZipCodeHelper.java
    checkstyle の指摘事項を修正。

--- src/main/java/zip_code_db_web_ui/app/zip_code/ZipCodeHelper.java
+++ src/main/java/zip_code_db_web_ui/app/zip_code/ZipCodeHelper.java
@@ -1,27 +1,28 @@
 package zip_code_db_web_ui.app.zip_code;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Component;
-
 import zip_code_db_web_ui.domain.model.ZipCode;
 
+/**
+ * リクエストパラメータからモデルオブジェクトを生成する。
+ */
 @Component
 public class ZipCodeHelper {
-    /**
-     * HttpServletRequest からリクエストパラメータを受け取って ZipCode へ変換して返す。
-     *
-     * @param request 検索フォームから送信された HttpServletrequest を受け取る。
-     * @return zipcode 変換された ZipCode を返す。
-     */
-    public ZipCode convertToZipCode(HttpServletRequest request) {
-        final ZipCode zipcode = new ZipCode();
+  /**
+   * {@link HttpServletRequest} からリクエストパラメータを受け取って {@link ZipCode} へ変換して返す。
+   *
+   * @param request 検索フォームから送信された {@link HttpServletRequest} を受け取る。
+   * @return zipcode 変換された {@link ZipCode} を返す。
+   */
+  public ZipCode convertToZipCode(HttpServletRequest request) {
+    final ZipCode zipcode = new ZipCode();
 
-        zipcode.setZipCode(request.getParameter("zipCode"));
-        zipcode.setPrefecture(request.getParameter("prefecture"));
-        zipcode.setCity(request.getParameter("city"));
-        zipcode.setArea(request.getParameter("area"));
+    zipcode.setZipCode(request.getParameter("zipCode"));
+    zipcode.setPrefecture(request.getParameter("prefecture"));
+    zipcode.setCity(request.getParameter("city"));
+    zipcode.setArea(request.getParameter("area"));
 
-        return zipcode;
-    }
+    return zipcode;
+  }
 }
