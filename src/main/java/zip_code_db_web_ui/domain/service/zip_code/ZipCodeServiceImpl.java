e917909a63811c1077076745b76fff4b982f2138
Jhonny Bravo <sanfranceshika5@gmail.com>
2021-08-20 01:00:47 +0900

    modified: src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceImpl.java
    CDI スコープを追記。

--- src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceImpl.java
+++ src/main/java/zip_code_db_web_ui/domain/service/zip_code/ZipCodeServiceImpl.java
@@ -1,10 +1,12 @@
 package zip_code_db_web_ui.domain.service.zip_code;
 
 import java.util.List;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import zip_code_db_web_ui.domain.model.ZipCode;
 import zip_code_db_web_ui.domain.repository.zip_code.ZipCodeRepository;
 
+@Stateless
 public class ZipCodeServiceImpl implements ZipCodeService {
   @Inject
   private ZipCodeRepository repository;
